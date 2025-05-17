c348e17582804b1fa998dc79cd01ef5c04b8353d
sudotac <sudo@tofuyard.net>
2025-05-17 19:51:19 +0900

    test: Add tests for exception handling

--- spec/temml_spec.rb
+++ spec/temml_spec.rb
@@ -2,6 +2,7 @@
 
 # SPDX-License-Identifier: CC0-1.0 AND MIT
 
+require 'execjs'
 require 'spec_helper'
 
 describe Temml do
@@ -17,5 +18,10 @@ describe Temml do
   it 'passes options to temml' do
     expect(Temml.render('c', displayMode: true)).to \
       include('<math display="block"')
+  end
+
+  it 'throw exception on error (or not)' do
+    expect { Temml.render('e^{i\pi') }.to raise_error(ExecJS::ProgramError)
+    expect { Temml.render('e^{i\pi', throwOnError: false) }.not_to raise_error
   end
 end
