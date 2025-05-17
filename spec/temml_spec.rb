74f3b1fc9fca618e10db36219a489f022ae511d7
sudotac <sudo@tofuyard.net>
2025-05-17 20:13:08 +0900

    test: Parse generated MathML by nokogiri

--- spec/temml_spec.rb
+++ spec/temml_spec.rb
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: CC0-1.0 AND MIT
 
 require 'execjs'
+require 'nokogiri'
 require 'spec_helper'
 
 describe Temml do
@@ -12,12 +13,14 @@ describe Temml do
   end
 
   it 'renders via temml' do
-    expect(Temml.render('c = \\pm\\sqrt{a^2 + b^2}')).to include('<math>')
+    frag = Nokogiri::HTML.fragment(Temml.render('c = \\pm\\sqrt{a^2 + b^2}'))
+    expect(frag.elements[0].name).to eq('math')
   end
 
-  it 'passes options to temml' do
-    expect(Temml.render('c', displayMode: true)).to \
-      include('<math display="block"')
+  it 'passes display option to temml' do
+    frag = Nokogiri::HTML.fragment(Temml.render('\int_0^1 \frac{dx}{1+x^2} = \frac{\pi}{4}', displayMode: true))
+    expect(frag.elements[0].name).to eq('math')
+    expect(frag.elements[0][:display]).to include('block')
   end
 
   it 'throw exception on error (or not)' do
